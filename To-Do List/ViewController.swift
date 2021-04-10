10679297815b446560e56c1825241614de0a2f19
Alexander Kudryashov <sasha080193@icloud.com>
2021-04-10 15:51:48 +0300

    Initial Commit

---
+++ To-Do List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do List
+//
+//  Created by Александр Кудряшов on 10.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
